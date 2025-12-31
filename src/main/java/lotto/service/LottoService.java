ef3e29664ead5e685868f22ac253002e78ca7279
sunJ0120 <sspure1214@gmail.com>
2025-12-31 14:46:48 +0900

    feat: domain 로직 조합할 service 마련

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,17 @@
 package lotto.service;
 
+import lotto.domain.*;
+
 public class LottoService {
+    public Lottos purchase(int purchaseAmount) {
+        return new Lottos(purchaseAmount/1000);
+    }
+
+    public WinningStats stats(Lottos lottos, WinningNumber winningNumber, WinningStats stats) {
+        for(Lotto lotto : lottos.getLottos()){
+            WinningInfo info = winningNumber.getWinningInfo(lotto);    // TODO : ENUM 조합하는 법 알아두기
+            stats.add(info);
+        }
+        return stats;
+    }
 }
