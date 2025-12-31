7977d7e7db142e1ff633a40b0dac4b85b23a4ddb
sunJ0120 <sspure1214@gmail.com>
2025-12-31 14:44:34 +0900

    feat: Lotto 도메인 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,57 @@
-package lotto;
+package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = numbers.stream().sorted().toList();
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDifferent(numbers);
+        for (Integer number : numbers) {
+            validateRange(number);
+        }
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1~45입니다.");
+        }
+    }
+
+    private void validateDifferent(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 전부 다 달라야 합니다.");
+        }
+    }
+
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
+
+    public int getContainCnt(Lotto other){
+        return (int) numbers.stream().filter(other::contains).count();
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public String toString(){
+        return numbers.toString();
+    }
 }
