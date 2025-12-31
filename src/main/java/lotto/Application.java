b62be54e2f2224652f99f88f9ac23557b0ebcf3e
sunJ0120 <sspure1214@gmail.com>
2025-12-31 14:48:27 +0900

    test: test 도메인 안으로 옮기기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+import lotto.domain.*;
+import lotto.service.LottoService;
+import lotto.util.InputParser;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView(new InputParser());
+        LottoService lottoService = new LottoService();
+        OutputView outputView = new OutputView();
+
+        // 1. 구입 > lotto print
+        int amount = inputView.inputPurchase();
+        System.out.println();
+        Lottos lottos = lottoService.purchase(amount);
+        outputView.printLottos(lottos);
+        System.out.println();
+
+        // 2. lotto 등수 맞추기
+        Lotto winningLotto = inputView.inputWinningLotto();
+        System.out.println();
+        BonusNum bonus = inputView.inputBonusNum();
+        System.out.println();
+
+        WinningNumber winningNumber = new WinningNumber(winningLotto, bonus);
+        WinningStats stats = new WinningStats();
+        stats = lottoService.stats(lottos, winningNumber, stats);
+
+        outputView.printStats(stats, amount);
     }
 }
