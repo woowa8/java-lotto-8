946b7f489e1de9edcf420dd60cc1206bf5d3e813
sunJ0120 <sspure1214@gmail.com>
2025-12-31 14:47:10 +0900

    feat: ,를 이용해서 쪼갤 parser 마련:

--- src/main/java/lotto/util/InputParser.java
+++ src/main/java/lotto/util/InputParser.java
@@ -1,4 +1,26 @@
 package lotto.util;
 
+import lotto.domain.Lotto;
+
+import java.util.Arrays;
+
 public class InputParser {
+
+    public Lotto parseNum(String input) {
+        String[] parsed = input.split(",");
+
+        for (String parseString : parsed) {
+            if (!parseString.matches("[0-9]+")) {
+                throw new NumberFormatException("[ERROR] 로또 번호는 반드시 숫자여야 합니다.");
+            }
+        }
+
+        try {
+            return new Lotto(Arrays.stream(parsed).map(Integer::parseInt).toList());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
 }
