fc880d4b56d320f405c79d05a3612cb8d9f1ffc9
sunJ0120 <sspure1214@gmail.com>
2025-12-31 14:46:25 +0900

    feat: 당첨 통계 계산할 클래스 구현

--- src/main/java/lotto/domain/WinningStats.java
+++ src/main/java/lotto/domain/WinningStats.java
@@ -1,4 +1,33 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WinningStats {
+    private final Map<WinningInfo, Integer> stats;
+
+    public WinningStats() {
+        this.stats = new HashMap<WinningInfo, Integer>();
+    }
+
+    public Double calculateROR(int purchaseMoney) {
+        double rate = (calculateTotalPrice() / (double) purchaseMoney) * 100;
+        return Math.round(rate * 10) / 10.0;  // TODO : 이거 원리 다시보기
+    }
+
+    // TODO : 여기 stream 사용하는거 다시 봐야 한다.
+    public Double calculateTotalPrice() {
+        return stats.entrySet().stream().mapToDouble(
+                entry -> entry.getKey().getPrize() * entry.getValue()
+        ).sum();
+    }
+
+    public Map<WinningInfo, Integer> add(WinningInfo winningInfo) {
+        stats.put(winningInfo, stats.getOrDefault(winningInfo, 0) + 1);
+        return stats;
+    }
+
+    public int getCount(WinningInfo winningInfo) {
+        return stats.getOrDefault(winningInfo, 0);
+    }
 }
