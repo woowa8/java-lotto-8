6165f4112b5f7107f268e64aa1184ec23a51cada
sunJ0120 <sspure1214@gmail.com>
2025-12-31 14:48:10 +0900

    feat: 출력에 사용할 outputView 마련

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,27 +2,36 @@ package lotto.view;
 
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
+import lotto.domain.WinningInfo;
+import lotto.domain.WinningStats;
 
-/*
-- [ ] 첫 번째 출력은 구매한 로또를 출력한다.
-- [ ] 두 번째 출력은 결과 통계를 출력한다.
-
-
-8개를 구매했습니다.
-[8, 21, 23, 41, 42, 43]
-[3, 5, 11, 16, 32, 38]
-[7, 11, 16, 35, 36, 44]
-[1, 8, 11, 31, 41, 42]
-[13, 14, 16, 38, 42, 45]
-[7, 11, 30, 40, 42, 43]
-[2, 13, 22, 32, 38, 45]
-[1, 3, 5, 14, 22, 45]
- */
 public class OutputView {
     public void printLottos(Lottos lottos) {
         System.out.println(lottos.getSize() + "개를 구매했습니다.");
-        for (Lotto lotto : lottos.getLottos()){
+        for (Lotto lotto : lottos.getLottos()) {
             System.out.println(lotto.toString());
         }
+    }
+
+    public void printStats(WinningStats winningStats, int purchasePrice) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        for (WinningInfo info : WinningInfo.values()) {
+            if("논외".equals(info.getRank())){
+                continue;
+            }
+            printInformation(info, winningStats);
+        }
+        System.out.println("총 수익률은 " + winningStats.calculateROR(purchasePrice) + "%입니다.");
+    }
+
+    private void printInformation(WinningInfo info, WinningStats winningStats) {
+        String money = String.format("%,d", info.getPrize());    // TODO : 숫자 끊어서 작성하는 방법 잊지말기
+        if (info.isBonus()) {
+            System.out.println(info.getNumberCnt() + "개 일치, 보너스 볼 일치 (" + money + "원) - " + winningStats.getCount(info) + "개");
+        }
+
+        System.out.println(info.getNumberCnt() + "개 일치 (" + money + "원) - " + winningStats.getCount(info) + "개");
     }
 }
