634ad13848df4c19aca12a510f307b5cbbdb9b7f
sunJ0120 <sspure1214@gmail.com>
2025-12-31 14:46:02 +0900

    feat: 당첨 번호를 저장할 클래스 마련

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,4 +1,18 @@
 package lotto.domain;
 
 public class WinningNumber {
+    private final Lotto winningLotto;
+    private final BonusNum bonus;
+
+    public WinningNumber(Lotto winningLotto, BonusNum bonus) {
+        this.winningLotto = winningLotto;
+        this.bonus = bonus;
+    }
+
+    public WinningInfo getWinningInfo(Lotto other) {
+        int count = winningLotto.getContainCnt(other);
+        boolean isBonus = winningLotto.contains(bonus.getBonusNum());
+
+        return WinningInfo.matchingScore(count, isBonus);
+    }
 }
